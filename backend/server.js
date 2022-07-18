662f86ac6f6ca4abf39c86ff5cc00b0ca83680c2
SkyeJackson <skyejackson@spelman.edu>
2022-07-17 23:15:46 -0400

    Return all services

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,8 @@ function addItem (request, response) {
 app.get("/get/items", getItems)
 //** week5, get all items from the json database*/
   function getItems (request, response) {
-    //begin here
-    
+    var data = fs.readFileSync('database.json');
+    response.json(JSON.parse(data));
   } 
 
 app.get("/get/searchitem",searchItems)
